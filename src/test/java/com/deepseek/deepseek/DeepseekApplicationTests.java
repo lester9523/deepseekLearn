258c1edbd126b2dcef97c525143b49d31126eef8
lester <17519539523@163.com>
2025-03-11 15:35:01 +0800

    first commit

---
+++ src/test/java/com/deepseek/deepseek/DeepseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepseek.deepseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
